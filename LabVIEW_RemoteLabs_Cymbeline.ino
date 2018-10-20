658edfcc2d10f745fd3dd5f299832c26a92cda67
KhyatiMehta3 <khyatimehta69@gmail.com>
2018-10-20 08:32:33 +0530

    Update LabVIEW_RemoteLabs_Cymbeline.ino

--- LabVIEW_RemoteLabs_Cymbeline.ino
+++ LabVIEW_RemoteLabs_Cymbeline.ino
@@ -22,8 +22,8 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 
-static const char ssid[] = "CYMB@BMSCE";
-static const char password[] = "PHASESHIFT";
+static const char ssid[] = ".......";
+static const char password[] = ".......";
 MDNSResponder mdns;
 int i;
 static void writeLED1(bool);
